30d0a9b81f9ff23266e0dde2f36cce791bf204fc
Ruslan Myznikov <ruslan.myznikov@nure.ua>
2018-08-13 14:48:21 +0300

    add fonts and more

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
-$(function() {
+window.addEventListener('DOMContentLoaded', function() {
+    var burgers = document.querySelectorAll(".burger-container");
+    for (var i = 0; i < burgers.length; i++) {
+        toggle.apply(burgers[i]);
+    }
 
-	// Custom JS
-
+    var control = document.getElementById("chk-control"), child;
+    control.indeterminate = true;
+    control.addEventListener('click', function() {
+        if (this.checked) {
+            for (let i = 0; i < burgers.length; i++) {
+                child = burgers[i].children[0];
+                child.classList.add(child.id + "-open");
+            }
+        }
+        else {
+            for (let i = 0; i < burgers.length; i++) {
+                child = burgers[i].children[0];
+                child.classList.remove(child.id + "-open");
+            }
+        }
+    })
 });
+
+function toggle() {
+    var menu = this.children[0];
+    this.addEventListener('click', function() {
+        menu.classList.toggle(menu.id + "-open");
+    });
+}
